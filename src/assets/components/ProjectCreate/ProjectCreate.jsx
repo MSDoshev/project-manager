fb56a04ae0f93d1b63394288f2d9f75b2513db2f
MSDoshev <mihaildo@outlook.com>
2024-02-13 12:43:56 +0200

    update ProjectCreate
    update the structure of the component

--- src/assets/components/ProjectCreate/ProjectCreate.jsx
+++ src/assets/components/ProjectCreate/ProjectCreate.jsx
@@ -3,16 +3,24 @@ import InputField from "./InputField";
 export default function ProjectCreate(){
 
     return(
-        
-        <div className="basis-3/4 p-11 text-3xl font-mono font-bold flex flex-col gap-4 items-center" >
-            <InputField label="Title" type='text'/>
-            <InputField label="Description" textarea type='text'/>
-            <InputField label="Due Date" type='date'/>
-            <div className="flex flex-row-reverse gap-7">
-            <button>Save</button>
-            <button>Cancel</button>
+              
+        <div className="w-[35rem] mt-16">
+            <div>
+                <InputField label="Title" type='text'/>
+                <InputField label="Description" textarea type='text'/>
+                <InputField label="Due Date" type='date'/>
             </div>
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                </li>
+                <li>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+                </li>
+            </menu>
         </div>
+      
+
         
     )
 
