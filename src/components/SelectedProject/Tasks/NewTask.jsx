04d0c5102671c9e213fcf4e5239fbd6bb0faaed4
MSDoshev <mihaildo@outlook.com>
2024-02-13 23:45:31 +0200

    Bug fixes
    fixed some small bugs

--- src/components/SelectedProject/Tasks/NewTask.jsx
+++ src/components/SelectedProject/Tasks/NewTask.jsx
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Modal from "../../Modal";
 
 export default function NewTask({onAdd}) {
-  const [enteredTask, setEnteredTask] = useState();
+  const [enteredTask, setEnteredTask] = useState('');
+
+  const modal = useRef();
 
   function handleChange(event) {
     setEnteredTask(event.target.value);
   }
 
   function handleClick() {
+    if (enteredTask.trim() === ''){
+        modal.current.open();
+        return;
+    }
     onAdd(enteredTask);
     setEnteredTask("");
   }
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-red-800 my-4">Invalid Input</h2>
+        <p className='text-slate-950 mb-4'>You cannot submit empty tasks.</p>
+    </Modal>
     <div className="flex items-center gap-4">
       <input
         type="text"
@@ -26,5 +38,6 @@ export default function NewTask({onAdd}) {
         Add Task
       </button>
     </div>
+    </>
   );
 }
